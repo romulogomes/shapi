ea1c3df93f088ed06def732c71cd85369587932d
Romulo Gomes <romulogomes13@hotmail.com>
2020-09-27 18:24:52 -0300

    versão nova

--- app/controllers/empregados_controller.rb
+++ app/controllers/empregados_controller.rb
@@ -2,6 +2,8 @@ class EmpregadosController < ApplicationController
   before_action :set_empregado, only: [:show, :update, :destroy]
 
   # FIXME: Romulo - 1 controller por tela
+  # FIXME: ROmulo - Empregados por empresa
+  # FIXME: ROmulo - Status do empregado
   def informacoes
     empregado = Empregado.find(params[:empregado_id])
     solicitacoes = Solicitacao.where(empregado_id: parametros[:empregado_id], mes_ano: parametros[:mes_ano])
@@ -38,7 +40,7 @@ class EmpregadosController < ApplicationController
 
   # POST /empregados
   def create
-    @empregado = Empregado.new(parametros["empregado"])
+    @empregado = Empregado.new(parametros)
 
     if @empregado.save
       render json: @empregado, status: :created, location: @empregado
@@ -49,7 +51,7 @@ class EmpregadosController < ApplicationController
 
   # PATCH/PUT /empregados/1
   def update
-    if @empregado.update(parametros["empregado"])
+    if @empregado.update(parametros)
       render json: @empregado
     else
       render json: @empregado.errors, status: :unprocessable_entity
@@ -62,10 +64,10 @@ class EmpregadosController < ApplicationController
   end
 
   private
-    # Use callbacks to share common setup or constraints between actions.
-    def set_empregado
-      @empregado = Empregado.find(params[:id])
-    end
+  # Use callbacks to share common setup or constraints between actions.
+  def set_empregado
+    @empregado = Empregado.find(params[:id])
+  end
 
     
 end
